5cc7960fc826c43d13859b56ac7e716faf702042
Leandro Lemes Vilela <leandrolvilela@gmail.com>
2023-06-18 22:55:26 -0300

    Construindo pesquisa e exclusão da aplicação

--- app_api/app.py
+++ app_api/app.py
@@ -58,3 +58,52 @@ def add_aplicacao(form:AplicacaoSchema):
         error_msg = "Não foi possível salva novo item :/"
         logger.warning(f"Erro ao adicionar a aplicação '{aplicacao.nome}', {error_msg}")
         return {"mesage": error_msg}, 400
+    
+@app.get('/aplicacao', tags=[aplicacao_tag],
+         responses={"200":AplicacaoSchema, "404": ErrorSchema})
+def get_produto(query: AplicacaoBuscaSchema):
+    """ Faz a busca por uma Aplicação a partir do id do produto
+        Retorna uma representação das aplicações
+    """ 
+    aplicacao_id = query.id
+    logger.debug(f"Coletando dados sobre produto #{aplicacao_id}")
+    # criando conexão com a base
+    session = Session()
+    # Fazendo a busca
+    aplicacao = session.query(Aplicacao).filter(Aplicacao.id == aplicacao_id).first()
+
+    if not aplicacao:
+        # Se a aplicação não foi encontrado
+        error_msg = "Aplicação não encontrado na base :/"
+        logger.warning(f"Erro ao buscar a aplicação '{aplicacao_id}', {error_msg}")
+        return {"message": error_msg}, 404
+    else:
+        logger.debug(f"Aplicação encontrado: '{aplicacao.nome}'")
+        # retorna a representação da aplicação
+        return apresenta_aplicacao(aplicacao), 200
+
+@app.delete('/aplicacao', tags=[aplicacao_tag],
+            responses={"200":AplicacaoDelSchema, "404": ErrorSchema})
+def del_aplicacao(query: AplicacaoBuscaSchema):
+    """Deleta uma aplicação a partir do nome da aplicação informado
+    
+    Retorna uma mensagem de configuração da remoção
+    """
+    aplicacao_nome = unquote(unquote(query.nome))
+    print(aplicacao_nome)
+    logger.debug(f"Deletando dados sobre a aplicação #{aplicacao_nome}")
+    # Criando conexão com a base
+    session = Session()
+    # Fazendo a remoção
+    count = session.query(Aplicacao).filter(Aplicacao.nome == aplicacao_nome).delete()
+    session.commit()
+
+    if count:
+        # Retorna a representação da mensagem de confirmação
+        logger.debug(f"Deletando aplicação #{aplicacao_nome}")
+        return {"message": "Aplicação removido", "id":aplicacao_nome}
+    else:
+        # Se a aplicação não foi encontrado
+        erro_msg = "Aplicação não encontrada na base :/"
+        logger.warning(f"Erro ao deletar a aplicação #'{aplicacao_nome}', {erro_msg}")
+        return {"message": erro_msg}, 404
